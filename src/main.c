3515970734b269372f2d17639beb31f3f161eedf
SamyAlderson <samysam35000@gmail.com>
2026-07-20 00:51:17 +0200

    had to read the compiler source to figure this out

--- src/main.c
+++ src/main.c
@@ -1,11 +1,8 @@
-/* Include necessary headers */
 #include <stdio.h>
-#include <stdlib.h>
 #include "bpf.h"
 #include "utils.h"
 
-/* Define the main function */
-int main(int argc, char **argv) {
-    /* Your code here */
+int main() {
+    printf("Hello World\n");
     return 0;
 }
