71acaa4e70574382216f1349ffe02f0332aaaac3
SamyAlderson <samysam35000@gmail.com>
2026-07-20 00:50:25 +0200

    this took way longer than it should have

--- src/main.c
+++ src/main.c
@@ -1,24 +1,11 @@
-/* Include stdlib.h for exit function */
+/* Include necessary headers */
+#include <stdio.h>
 #include <stdlib.h>
-
-/* Include bpf.h for BPF definitions */
-#include <bpf.h>
-
-/* Include utils.h for custom functions */
+#include "bpf.h"
 #include "utils.h"
 
-int main() {
-    /* Initialize BPF */
-    bpf_init();
-
-    /* Call custom function */
-    my_custom_function();
-
-    /* Exit with success code */
-    exit(EXIT_SUCCESS);
-}
-
-/* Define custom function */
-void my_custom_function() {
-    /* Your custom function implementation */
+/* Define the main function */
+int main(int argc, char **argv) {
+    /* Your code here */
+    return 0;
 }
