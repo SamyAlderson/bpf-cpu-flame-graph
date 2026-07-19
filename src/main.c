80edc1195ec482ee157b06c7821094317547b3e8
SamyAlderson <samysam35000@gmail.com>
2026-07-20 00:48:39 +0200

    learned something about the language while fixing this

--- src/main.c
+++ src/main.c
@@ -1,37 +1,17 @@
-// Include necessary header files
+/* Include the necessary header files */
 #include <stdio.h>
 #include <stdlib.h>
-#include "bpf.h"
-#include "utils.h"
+#include "src/bpf.h"
+#include "src/utils.h"
 
-// Function to print usage
-void print_usage(void) {
-    printf("Usage: %s <options>\n", argv[0]);
+/* Function to handle CPU flame graph generation */
+void generate_flame_graph(void)
+{
+    // Your code here
 }
 
-int main(int argc, char **argv) {
-    // Initialize variables
-    int opt;
-    char *optarg;
-
-    // Parse command line options
-    while ((opt = getopt(argc, argv, "h")) != -1) {
-        switch (opt) {
-            case 'h':
-                print_usage();
-                return 0;
-            default:
-                fprintf(stderr, "Invalid option: %c\n", opt);
-                return 1;
-        }
-    }
-
-    // Call bpf_init function
-    if (bpf_init() < 0) {
-        fprintf(stderr, "Failed to initialize BPF\n");
-        return 1;
-    }
-
-    // Call main function
-    return main_func();
+int main(int argc, char **argv)
+{
+    generate_flame_graph();
+    return 0;
 }
